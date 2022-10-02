d274bc88d94972ca6fc987101a8d8f7b87297405
glattanzio <gonzalattanzio@gmail.com>
2022-10-02 16:00:39 -0300

    Borro Imports de CursoController

--- src/main/java/com/example/demo/controller/CursoController.java
+++ src/main/java/com/example/demo/controller/CursoController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.CursoService;
-import com.example.demo.service.MaterialService;
 import com.example.demo.entity.Curso;
-import com.example.demo.entity.Material;
 
 @RestController
 
