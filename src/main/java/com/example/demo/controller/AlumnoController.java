8f1bd039752bd4dc9895668b6c9093fc34c6da31
FedeTusa <fedetusa01@gmail.com>
2022-10-02 15:53:38 -0300

    agrego el back de alumnos

--- src/main/java/com/example/demo/controller/AlumnoController.java
+++ src/main/java/com/example/demo/controller/AlumnoController.java
@@ -1,1 +1,48 @@
 package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.AlumnoService;
+import com.example.demo.entity.Alumno;
+
+@RestController  
+
+public class AlumnoController {
+	@Autowired	
+	AlumnoService alumnoService;
+	
+	@RequestMapping(value = "/alumnos", method = RequestMethod.GET, produces = "application/json")
+	public List<Alumno> getAlumnos() {
+		return alumnoService.findAllAlumno();
+	}
+	
+	
+	@RequestMapping(value = "/alumnos/{id}", method = RequestMethod.GET, produces = "application/json")
+	public Optional<Alumno> getAlumno(@PathVariable Long id) {
+		return alumnoService.findAlumnoById(id);
+	}
+
+	
+	@RequestMapping(value = "/alumnos/add", method = RequestMethod.POST, produces = "application/json")
+	public Alumno addPostPost(Alumno alumno) {
+		return alumnoService.saveAlumno(alumno);
+	}
+
+	@RequestMapping(value = "/alumnos/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
+	public String deletePost(@PathVariable Long id) {
+		return alumnoService.deleteAlumno(id);
+	}
+	
+	@RequestMapping(value = "/alumnos/update", method = RequestMethod.PUT, produces = "application/json")
+	public String updateAlumno(Alumno alumno) {
+		return alumnoService.updateAlumno(alumno);
+	}
+	
+}
---
+++ src/main/java/com/example/demo/service/AlumnoServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Alumno;
+import com.example.demo.repository.AlumnoRepository;
+
+@Service
+public class AlumnoServiceImpl implements AlumnoService {
+
+	@Autowired
+	AlumnoRepository alumnoRepository; 
+									
+
+	@Override
+	public List<Alumno> findAllAlumno() {
+		return alumnoRepository.findAll();
+	}
+
+	@Override
+	public Optional<Alumno> findAlumnoById(Long id) {
+		return alumnoRepository.findById(id);
+	}
+
+	@Override
+	public Alumno saveAlumno(Alumno newAlumno) {
+		return alumnoRepository.save(newAlumno);
+	}
+
+	@Override
+	public String deleteAlumno(Long id) {
+		if (alumnoRepository.findById(id) != null) {
+			alumnoRepository.deleteById(id);
+			return "OK";
+		}
+		return "ERROR: el id no existe";
+	}
+
+	@Override
+	public String updateAlumno(Alumno alumno) {
+		if (alumnoRepository.findById(alumno.getId()) != null) {
+			alumnoRepository.save(alumno);
+			return "OK";
+		}
+
+		return "ERROR: el id no existe";
+	}
+
+	
+
+}
