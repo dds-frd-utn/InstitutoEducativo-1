78cac55ea0ee868537339100933eb68bb4f6e981
FedeTusa <fedetusa01@gmail.com>
2022-10-05 21:40:14 -0300

    agrego el stock en cursos

--- src/main/java/com/example/demo/entity/Material.java
+++ src/main/java/com/example/demo/entity/Material.java
@@ -13,7 +13,7 @@ import javax.persistence.ManyToOne;
 @Entity
 @Table(name = "material")
 public class Material {
-	@Id 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	Long id;
@@ -21,34 +21,50 @@ public class Material {
 	String titulo;
 	@Column(name = "costo")
 	Long costo;
-    @JoinColumn(name = "id_curso")
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    private Curso curso;
+	@JoinColumn(name = "id_curso")
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	private Curso curso;
+	@Column(name = "stock")
+	Long stock;
 
-		
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public Long getCosto() {
 		return costo;
 	}
+
 	public void setCosto(Long costo) {
 		this.costo = costo;
 	}
-	
+
 	public Curso getCurso() {
 		return curso;
 	}
+
 	public void setCurso(Curso curso) {
 		this.curso = curso;
 	}
+
+	public Long getStock() {
+		return stock;
+	}
+
+	public void setStock(Long stock) {
+		this.stock = stock;
+	}
+
 }
